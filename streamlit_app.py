d96ddfedcbe8f9e3480ca731f241110c6f59857e
jjhxiao <144857920+jjhxiao@users.noreply.github.com>
2023-11-06 14:10:29 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
 import streamlit as sl
-import snowflake.connector as sgc
+import snowflake.connector as sfc
+
+my_cnx = sfc.connect(**sl.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+sl.text("Hello from Snowflake:")
+sl.text(my_data_row)
